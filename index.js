5b998f4dd017fa27ce4db6cf8f92f39b02ac8140
Saqib Ahmad <you@example.com>
2022-10-29 11:39:13 +0600

    Users data created

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+const users = [
+    { id: 1, name: "Saqib", email: "saqib00@gmail.com" },
+    { id: 1, name: "Soleh", email: "soleh00@gmail.com" },
+    { id: 1, name: "Saad", email: "saad00@gmail.com" }
+]
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
 
 app.get('/', (req, res) => {
     res.send("Simple node server running");
